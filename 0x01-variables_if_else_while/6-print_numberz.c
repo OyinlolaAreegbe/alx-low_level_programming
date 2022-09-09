44c41f7641d72c2e6354510a45437ee0f4fc9598
OyinlolaAreegbe <oyinlola.areegbe.com>
2022-09-09 16:44:46 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - prints all single digit numbers of base 10 starting from 0, only use putchar
+ * main - prints all single digit numbers of base 10 starting from 0
  *
  * Return: Always 0 (Success)
  */
@@ -15,6 +15,6 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 		putchar(num);
 	putchar('\n');
-	
+
 	return (0);
 }
