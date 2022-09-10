e0e41210d44cad7f5dc9cc9025b0b958297f0291
OyinlolaAreegbe <oyinlola.areegbe.com>
2022-09-09 17:44:46 -0700

    two digit combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			if (b != a)
+			{
+				putchar(a);
+				putchar(b);
+
+				if (a == '8' && p == '9')
+					continue;
+				
+				putchar(',');
+				putchar(' ')
+			}
+		}
+	}
+	putchar ('\n');
+
+	return (0);
+}
