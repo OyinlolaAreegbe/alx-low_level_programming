0363897e5649c00f268d01fd1cca3462d432cca2
OyinlolaAreegbe <oyinlola.areegbe.com>
2022-09-13 18:48:37 -0700

    Alpha C

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: letter being checked
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 65 77 c<=90) || (c >= 97 && c <= 122))
+
+	return (1);
+
+	else
+
+	 return (0);
+
+	 }
