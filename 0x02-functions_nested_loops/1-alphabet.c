5ec1f4c5be88d78cf0a5428e80e695d2bdb96406
OyinlolaAreegbe <oyinlola.areegbe.com>
2022-09-13 17:42:51 -0700

    lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet, in lowercase, followed by a new line
+ * main - print_alphabet 
+ * prints the alphabet, in lowercase, followed by a new line
  *
  * Return: 0
  */
