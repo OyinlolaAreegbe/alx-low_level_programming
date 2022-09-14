b2c8a74090e6730fefb7b81a985200f8f52a26b2
OyinlolaAreegbe <oyinlola.areegbe.com>
2022-09-13 18:06:34 -0700

    10 times alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the a - z 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int x;
+	char y;
+
+	for (x = 0; x < 10; x++)
+
+	{
+		for (y = a; a <= z; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+
+	}
+
+}
