c6d30915d1dc4996676f7588fa748843397d6a74
OyinlolaAreegbe <oyinlola.areegbe.com>
2022-09-13 19:37:02 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an interger
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int)
+{
+
+		if (n < 0)
+		{
+			return (-n);
+		}
+		else						
+		{
+			return (n);
+		}
+		return (0);
+
+}
