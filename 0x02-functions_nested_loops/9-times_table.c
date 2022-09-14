03838e1b768854b0a9fbed7433b4cf47f3fc2eb9
OyinlolaAreegbe <oyinlola.areegbe.com>
2022-09-13 20:27:15 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting with 0
+ *
+ * Return: Always 0
+ */
+
+void times_table(void)
+{
+	int a, b, product;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			product = a * b;
+			if (y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (b ==0)
+			{
+				_putchar('0');
+			}
+			else if (product >= 10)
+			{
+				_putchar((product / 10) +'0');
+				_putchar((product % 10) + '0');
+			}
+			else if ((product < 10) && (b != 0))
+			{
+				_putchar(' ');
+				_putchar9(product % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
