5b01ba3f6c6ece188abf33a729da96c435b6e2ae
OyinlolaAreegbe <oyinlola.areegbe.com>
2022-09-15 08:53:05 -0700

    two mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer
- * is positive or negative
+ * main - tests function if integer is positive or negative
  *
  * Return: Always 0 (Success)
  */
