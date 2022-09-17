cb39ebadfcf8b9500c608f50b1ca72bd12f1baf6
OyinlolaAreegbe <oyinlola.areegbe.com>
2022-09-16 20:54:41 -0700

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prime numbers
+ * find the largest prime number of 612852475143
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+unsigned int long n = 612852475143, a = (int) sqrt(n);
+
+while (1)
+{
+	if (n % a == 0)
+	{
+		printf("%lu \n", n / a);
+		break;
+
+	}
+	a--;
+
+}
+return (0);
+
+}
